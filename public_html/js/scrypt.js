2fe5e9ba71771825eada602ffd2c51401fe18112
Vasyl Ignatyev <vignatyev@localhost.localdomain>
2020-04-03 10:17:35 +0300

    03.04.20

--- public_html/js/scrypt.js
+++ public_html/js/scrypt.js
@@ -1,38 +1,101 @@
-console.log("Scrypt running...");
-const draggable = document.getElementById('draggable');
+"use strict";
+
+let placeHoleder = null;
+
 const itemList = document.getElementById('item_list');
 
-function onDragStart(e) {
-    console.log("Drag Start");
-    e.dataTransfer.setData("text", e.target.id);
+function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
     
-    itemList.classList.add("droppable");
-    e.target.style.opacity = "0.2";
-    e.target.style["border-style"] =  "dotted";
+const createPlaceholder= function () {
+    const placeholder = document.createElement("li");
+    placeholder.className = "placeholder droppable drop_zone";
+    placeholder.addEventListener("drop", onDrop);
+    placeholder.addEventListener("dragover", onDragOver);
+    return placeholder;
+}
+    
+function onDragStart(e) {
+    console.log("onDragStart");
+    e.target.style.opacity = 0.2;
+    e.target.style.backgroundColor = "#FFFFFF";
+
+    e.dataTransfer.setData("text/html", e.target.innerHTML);
+    const itemList = document.getElementById("item_list");
+    itemList.style.border = "2px dashed gray";
+    if(itemList.childElementCount > 0) {
+        itemList.childNodes.forEach((item) => {
+            if(item && item.classList) {
+                item.classList.add("event_blind");
+            }
+        });
+//        return false;
+    }
+    placeHoleder = createPlaceholder();
+    itemList.append(placeHoleder);
 }
 
-function onDragEnd(e) {
-    console.log("Drag End");
-    itemList.classList.remove("droppable");
-    e.target.style.opacity = "1";
-    e.target.style["border-style"] =  "solid";
-}
-
-function onDragOver(e) {
-    //console.log(e);
+ function onDrop(e) {
     e.preventDefault();
+    
+    const dragged = e.dataTransfer.getData("text/html");
+    console.log(dragged);
+    const li = document.createElement('li');
+    li.innerHTML = dragged.trim();
+    li.classList.add("item");
+    li.firstChild.setAttribute("contenteditable", true);
+    e.target.replaceWith(li);
+    if(placeHoleder) {
+        placeHoleder.remove();
+        
+    }
+    placeHoleder = null;
+    li.addEventListener("dragenter", onDragEnter);
+}
+    
+function onDragEnd(e) {
+    console.log("onDragEnd");
+    e.target.style.opacity = 1;
+    e.target.style["background-color"] = null;
+    if(placeHoleder) {
+        placeHoleder.remove();
+        placeHoleder = null;
+    }
+    const itemList = document.getElementById("item_list");
+    itemList.style.border = null;
+    itemList.childNodes.forEach((item) => {
+        if(item && item.style && item.style.classList) {
+            item.style.classList.remove("event_blind");
+        }
+    });
 }
 
-function onDrop(e) {
-    const ul = e.target.closest("ul");
-    const data = e.dataTransfer.getData("text");
-    const item = document.getElementById(data);
-    item.style.opacity = "1";
-    item.style["border-style"] =  "solid";
-    ul.appendChild(item.cloneNode(true));
+const onDragOver = function(e) {
+    //Allow Drtop Event
+    e.preventDefault(); // Necessary. Allows us to drop.
+    e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+    return true;
+};
+    
+const onDragEnter = function(e) {
+    if(placeHoleder) {
+        placeHoleder.remove();
+        placeHoleder = null;
+    }
+    e.preventDefault();
+    console.log("onDragEnter");
+    const li = e.target.closest("li");
+    
+    placeHoleder = createPlaceholder();
+    insertAfter(placeHoleder, li);
+    
+
+    return false;
 }
 
-draggable.addEventListener('dragstart', onDragStart, false);
-draggable.addEventListener('dragend', onDragEnd, false);
-itemList.addEventListener('drop', onDrop, false)
-itemList.addEventListener('dragover', onDragOver, false);
+const onDragLeave = function(e) {
+    e.preventDefault();
+    console.log("onDragLeave");
+    e.target.style["padding-bottom"] = null;
+}
