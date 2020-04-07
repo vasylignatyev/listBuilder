6283c8154f647f291c14c3714ebe88db5a04ac3c
vasylignatyev <vasyl.ignatyev@gmail.com>
2020-04-07 07:48:49 +0300

    07.04.2020

--- public_html/js/scrypt.js
+++ public_html/js/scrypt.js
@@ -1,94 +1,114 @@
 "use strict";
 
-let placeHoleder = null;
-
-const itemList = document.getElementById("item_list");
-
-const insertAfter = function (newNode, existingNode) {
-    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
-};
-
-const createPlaceholder= function () {
-    const placeholder = document.createElement("li");
-    placeholder.className = "placeholder droppable drop_zone";
-    placeholder.addEventListener("drop", onDrop);
-    placeholder.addEventListener("dragover", onDragOver);
-    return placeholder;
-};
-
-const onDragStart = function (e) {
-    e.target.style.opacity = 0.2;
-    e.target.style.backgroundColor = "#FFFFFF";
-
-    e.dataTransfer.setData("text/html", e.target.innerHTML);
+const app = {
+    placeHoleder: null,
     
-    placeHoleder = createPlaceholder();
-    itemList.style.border = "2px dashed gray";
-    if(itemList.childElementCount > 0) {
-        itemList.childNodes.forEach( item => {
-            if(item && item.classList) {
-                item.classList.add("event_blind");
+    itemList: document.getElementById("item_list"),
+    
+    insertAfter: function (newNode, existingNode) {
+        existingNode.parentNode && existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+    },
+    
+    createPlaceholder: function () {
+        const placeholder = document.createElement("li");
+        placeholder.className = "placeholder droppable drop_zone";
+        placeholder.addEventListener("drop", (e) => this.onDrop(e));
+        placeholder.addEventListener("dragover", (e) => this.onDragOver(e));
+        return placeholder;
+    },
+                                                                                                                                            
+    onDragStart: function (e) {
+        e.target.style.opacity = 0.2;
+        e.target.style.backgroundColor = "#FFFFFF";
+
+        e.dataTransfer.setData("text/html", e.target.innerHTML);
+
+        this.placeHoleder = this.createPlaceholder();
+        this.itemList.style.border = "2px dashed gray";
+        if(this.itemList.childElementCount > 0) {
+            this.itemList.childNodes.forEach( item => {
+                if(item && item.classList) {
+                    item.firstChild.setAttribute("contenteditable", false);
+                }
+            });
+            this.itemList.insertBefore(this.placeHoleder, this.itemList.firstChild);
+        } else {
+            this.itemList.append(this.placeHoleder);
+        }
+        return false;
+    },
+    
+    onDrop: function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        const dragged = e.dataTransfer.getData("text/html");
+        
+        const li = document.createElement("li");
+        li.innerHTML = dragged.trim();
+        li.classList.add("item");
+        li.firstChild.setAttribute("contenteditable", true);
+        li.addEventListener("drop", (e) => this.onDrop(e));
+        li.addEventListener("dragover", (e) => this.onDragOver(e));
+        li.addEventListener("dragenter", (e) => this.onDragEnter(e));
+        this.placeHoleder.replaceWith(li);
+
+        if(this.placeHoleder) {
+            this.placeHoleder.remove();
+            this.placeHoleder = null;
+        }
+        return false;
+    },
+    
+    onDragEnd: function (e) {
+        e.target.style.opacity = 1;
+        e.target.style["background-color"] = null;
+        if(this.placeHoleder) {
+            this.placeHoleder.remove();
+            this.placeHoleder = null;
+        }
+        this.itemList.style.border = null;
+        this.itemList.childNodes.forEach( function(item) {
+            if(item && item.firstChild){
+                item.firstChild.setAttribute("contenteditable", true);
             }
         });
-        itemList.insertBefore(placeHoleder, itemList.firstChild);
-    } else {
-        itemList.append(placeHoleder);
-    }
-    return false;
-};
-
-const onDrop = function (e) {
-    e.preventDefault();
-    const dragged = e.dataTransfer.getData("text/html");
-    const li = document.createElement("li");
-    li.innerHTML = dragged.trim();
-    li.classList.add("item");
-    li.firstChild.setAttribute("contenteditable", true);
-    e.target.replaceWith(li);
-    if(placeHoleder) {
-        placeHoleder.remove();
-    }
-    placeHoleder = null;
-    li.addEventListener("dragenter", onDragEnter);
-    return false;
-};
-
-const onDragEnd = function (e) {
-    e.target.style.opacity = 1;
-    e.target.style["background-color"] = null;
-    if(placeHoleder) {
-        placeHoleder.remove();
-        placeHoleder = null;
-    }
-    itemList.style.border = null;
-    itemList.childNodes.forEach( item => {
-        if(item && item.style && item.style.classList) {
-            item.style.classList.remove("event_blind");
+        return false;
+    },
+    
+    onDragOver: function(e) {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = "move";
+        const YY= 2 * e.target.getBoundingClientRect().top;
+        const y = e.clientY - YY;  //y position within the element.
+        
+        if(e.target === this.placeHoleder) {
+            return false;
         }
-    });
-    return false;
-};
+        if (this.placeHoleder && y > 0) {
+            //e.target.insertAfter(this.placeHoleder. this.target);
+        } else {
+            //e.target.parentNode && e.target.parentNode.insertBefore(this.placeHoleder, e.target);
+        }
 
-const onDragOver = function(e) {
-    e.preventDefault();
-    e.dataTransfer.dropEffect = "move";
-    return false;
-};
+        return false;
+    },
+    
+    onDragEnter: function(e) {
+        e.preventDefault();
 
-const onDragEnter = function(e) {
-    if(placeHoleder) {
-        placeHoleder.remove();
-        placeHoleder = null;
+        if(this.placeHoleder) {
+            this.placeHoleder.remove();
+            this.placeHoleder = null;
+        }
+        const li = e.target.closest("li");
+        this.placeHoleder = this.createPlaceholder();
+        this.insertAfter(this.placeHoleder, li);
+        return false;
+    },
+
+    onDragLeave: function(e) {
+        e.preventDefault();
+        e.target.style["padding-bottom"] = null;
+        return false;
     }
-    e.preventDefault();
-    const li = e.target.closest("li");
-    placeHoleder = createPlaceholder();
-    insertAfter(placeHoleder, li);
-    return false;
-};
-
-const onDragLeave = function(e) {
-    e.preventDefault();
-    e.target.style["padding-bottom"] = null;
-    return false;
 };
